e741994f356832db1181fa4e39e61f5d4af43a72
Amaankhan17 <amaankhan134@eng.rizvi.edu.in>
2026-02-11 02:06:10 +0530

    Update comments and add factorial calculation logic

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Amaan Khan
+# Date:30/01/2026
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+num = int(input("Enter Number: "))
+
+if num < 0:
+    print(f"Factorial of {num} is Not Defined")
+else:
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"Factorial of {num} is {factorial}")
+
